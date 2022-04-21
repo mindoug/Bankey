6f97a36ad8b0fb53f156791f2ffa14d46f019df0
Mindy Douglas <mindy.douglas@icloud.com>
2022-04-21 09:15:22 -0500

    add table view header

---
+++ Bankey/AccountSummary/AccountSummaryHeaderView.swift
@@ -0,0 +1,51 @@
+//
+//  AccountSummaryHeaderView.swift
+//  Bankey
+//
+//  Created by Mindy Douglas on 4/21/22.
+//
+
+import UIKit
+
+class AccountSummaryHeaderView: UIView {
+    
+    @IBOutlet var contentView: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: UIView.noIntrinsicMetric, height: 144)
+    }
+    
+    private func commonInit() {
+        let bundle = Bundle(for: AccountSummaryHeaderView.self)
+        bundle.loadNibNamed("AccountSummaryHeaderView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.backgroundColor = appColor
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        contentView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+}
+
+
+// intrinsic content size is size view wants to give itself by default
+// lets it know how big the size wants to be 
+
+// pass name of class as parameter into bundle
+// tell which nib to load by passing in name of file as string
+// set owner to self
+// take contentView from nib and adds to subView
+// set background color
+// pin contentView to all 4 edges
